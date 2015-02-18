090b697ae61d5a44f2ed47d08424d1e7352018dd
Paul Holser <pholser@alumni.rice.edu>
2015-02-18 08:59:09 -0600

    Problem 12 in Haskell

--- PrimeFactors.hs
+++ PrimeFactors.hs
@@ -1,17 +1,19 @@
 module PrimeFactors (primeFactors, isPrime, nthPrime) where
 
 import Data.List
+import Data.Function.Memoize
 
 primeFactors :: Integer -> [Integer]
 primeFactors 1 = []
 primeFactors 2 = [2]
 primeFactors 3 = [3]
 primeFactors n = factors n []
-    where factors n fs =
+    where factors' n fs =
              let candidate = find (\x -> n `mod` x == 0) [2..floor.sqrt $ fromIntegral n]
              in case candidate of
                  Nothing -> fs ++ [n]
-                 Just f -> factors (n `div` f) (fs ++ [f])
+                 Just f -> factors' (n `div` f) (fs ++ [f])
+          factors = memoize factors'
 
 isPrime :: Integer -> Bool
 isPrime n = length (primeFactors n) == 1
--- problem12.hs
+++ problem12.hs
@@ -1,6 +1,18 @@
-triangleNumber :: Integer -> Integer
-triangleNumber 1 = 1
-triangleNumber n = n + triangleNumber (n - 1)
+import PrimeFactors
+import Data.Map (fromListWith, toList)
+import Data.Function.Memoize
 
-factors :: Integer -> [Integer]
-factors n = [f | f <- [1..n], n `mod` f == 0]
+triangleNumber' :: Integer -> Integer
+triangleNumber' 1 = 1
+triangleNumber' n = n + triangleNumber' (n - 1)
+
+triangleNumber = memoize triangleNumber'
+
+numberOfFactors :: Integer -> Integer
+numberOfFactors n =
+    let occur = Data.Map.fromListWith (+) [(f, 1) | f <- primeFactors n]
+        occurrences = toList occur
+        exponents = Prelude.map snd occurrences
+    in product $ Prelude.map (+1) exponents
+
+problem12 n = (filter (\t -> numberOfFactors t > n) (Prelude.map triangleNumber [1..])) !! 0
